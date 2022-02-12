6ddd477e44282ebbbc26a33428fda91088abf072
Marek Prochazka <m.prochazka2002@gmail.com>
2022-02-12 09:19:57 +0100

    update

--- build.bat
+++ build.bat
@@ -1,1 +1,9 @@
 xcopy /e /Y src\django_dart_reverse django-dart-reverse\django_dart_reverse
+
+CALL venv\Scripts\activate.bat
+
+cd django-dart-reverse
+
+CALL python setup.py sdist
+
+CALL python setup.py bdist_wheel
